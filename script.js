256e89b0d4b4139324d75c9a4669be0d9e5a52ed
Belma Korlat <137005711+BelmaKorlat@users.noreply.github.com>
2023-10-06 21:58:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let kontej = document.getElementById("kontejner");
+let span = document.getElementById("prvi");
+
+function klik1() {
+    kontej.style.backgroundImage = "url(slika1.jpeg)";
+    span.innerHTML = "1 / 3";
+    document.getElementById("dugme1").style.backgroundColor = "rgb(244, 243, 238)";
+    document.getElementById("dugme2").style.backgroundColor = "rgb(89, 87, 88)";
+    document.getElementById("dugme3").style.backgroundColor = "rgb(89, 87, 88)";
+}
+
+function klik2() {
+    kontej.style.backgroundImage = "url(slika2.jpeg)";
+    span.innerHTML = "2 / 3";
+    document.getElementById("dugme1").style.backgroundColor = "rgb(89, 87, 88)";
+    document.getElementById("dugme2").style.backgroundColor = "rgb(244, 243, 238)";
+    document.getElementById("dugme3").style.backgroundColor = "rgb(89, 87, 88)";
+}
+
+function klik3() {
+    kontej.style.backgroundImage = "url(slika3.jpeg)";
+    span.innerHTML = "3 / 3";
+    document.getElementById("dugme1").style.backgroundColor = "rgb(89, 87, 88)";
+    document.getElementById("dugme2").style.backgroundColor = "rgb(89, 87, 88)";
+    document.getElementById("dugme3").style.backgroundColor = "rgb(244, 243, 238)";
+}
+
+var brojac = 1;
+
+function lijevo() {
+    if (document.getElementById("dugme1").style.backgroundColor == "rgb(244, 243, 238)") {
+        klik3();
+    }
+
+    else if (document.getElementById("dugme2").style.backgroundColor == "rgb(244, 243, 238)") {
+        klik1();
+    }
+
+    else if (document.getElementById("dugme3").style.backgroundColor == "rgb(244, 243, 238)") {
+        klik2();
+    }
+}
+
+function desno() {
+    if (document.getElementById("dugme1").style.backgroundColor == "rgb(244, 243, 238)") {
+        klik2();
+    }
+
+    else if (document.getElementById("dugme2").style.backgroundColor == "rgb(244, 243, 238)") {
+        klik3();
+    }
+
+    else if (document.getElementById("dugme3").style.backgroundColor == "rgb(244, 243, 238)") {
+        klik1();
+    }
+}
+
+klik1();
